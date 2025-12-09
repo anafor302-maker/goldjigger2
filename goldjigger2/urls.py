51fe1d436d6ebd7616d47171b47333d9207e99aa
anafor302-maker <anafor302@gmail.com>
2025-12-09 20:10:52 +0300

    hata aldık

--- goldjigger2/urls.py
+++ goldjigger2/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path, include
 
 
+def home(request):
+    return JsonResponse({"message": "API running!"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
     path('api/v1/cocktails/', include('cocktails.urls')),
 ]
